952a6c5f08cdebccac926cd1cca0722790004508
Keah Peters <keahpeters@hotmail.co.uk>
2017-10-10 14:03:11 +0100

    Added unit tests

---
+++ BudgetPlanner.Tests/ViewComponentTests/LoginLogoutViewComponentTests.cs
@@ -0,0 +1,37 @@
+﻿using BudgetPlanner.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using NUnit.Framework;
+
+namespace BudgetPlanner.Tests.ViewComponentTests
+{
+    [TestFixture]
+    public class LoginLogoutViewComponentTests
+    {
+        private LoginLogoutViewComponent viewComponent;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.viewComponent = new LoginLogoutViewComponent();
+        }
+
+        [Test]
+        public void WhenInvokeIsCalledThenUseDefaultView()
+        {
+            IViewComponentResult result = this.viewComponent.Invoke();
+
+            Assume.That(result, Is.TypeOf(typeof(ViewViewComponentResult)));
+
+            Assert.That(((ViewViewComponentResult) result).ViewName, Is.Null);
+        }
+
+        [Test]
+        public void WhenInvokeIsCalledThenViewComponentResultIsReturned()
+        {
+            IViewComponentResult result = this.viewComponent.Invoke();
+
+            Assert.That(result, Is.TypeOf(typeof(ViewViewComponentResult)));
+        }
+    }
+}
